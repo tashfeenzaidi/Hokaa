f8a61e5633fd047edbd40cc4c86a70aac7fb4f70
salarzaidi <salar.zd@gmail.com>
2022-04-01 13:12:32 +0500

    refresh list add on history screen

--- lib/Orders/ordershistory.dart
+++ lib/Orders/ordershistory.dart
@@ -92,99 +92,106 @@ class _OrderHistoryState extends State<OrderHistory> {
                 children: [
                   Container(margin: EdgeInsets.only(top: 15), child: PageTopHeading('MY ORDERS')),
                   Expanded(
-                      child: PagedListView(
+                      child: RefreshIndicator(
+                        color: primaryColor,
+                        onRefresh: () => Future.sync(
+                              () => _pagingController.refresh(),
+                        ),
+                        child: PagedListView(
                     builderDelegate:
-                        PagedChildBuilderDelegate<Data>(itemBuilder: (context, item, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 6.0),
-                        child: GestureDetector(
-                            onTap: () {
-                              Get.toNamed("/orderDetailsNew", arguments: item);
-                            },
-                            child: Container(
-                                height: 120,
-                                padding: EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  color: cardBackgroundColor,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            Utils.getDate(DateTime.parse(item.createdAt!)),
-                                            style: Theme.of(context)
-                                                .textTheme
-                                                .subtitle1!
-                                                .copyWith(color: primaryColor, fontSize: 14),
-                                          ),
-                                          Icon(
-                                            Icons.arrow_forward_ios_rounded,
-                                            color: primaryColor,
-                                            size: 18,
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            "Order ID",
-                                            style: historyTextstyle,
-                                          ),
-                                          Text(
-                                            '${item.id!}',
-                                            style: historyTextstyle,
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            "Status",
-                                            style: historyTextstyle,
-                                          ),
-                                          Row(
-                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Icon(
-                                                Icons.cancel,
-                                                color: Colors.red,
-                                                size: 15,
-                                              ),
-                                              Padding(
-                                                padding: EdgeInsets.only(left: 10.0),
-                                                child: Text(
-                                                  item.status!,
-                                                  style: historyTextstyle,
+                          PagedChildBuilderDelegate<Data>(itemBuilder: (context, item, index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 6.0),
+                          child: GestureDetector(
+                              onTap: () {
+                                Get.toNamed("/orderDetailsNew", arguments: item);
+                              },
+                              child: Container(
+                                  height: 120,
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color: cardBackgroundColor,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              Utils.getDate(DateTime.parse(item.createdAt!)),
+                                              style: Theme.of(context)
+                                                  .textTheme
+                                                  .subtitle1!
+                                                  .copyWith(color: primaryColor, fontSize: 14),
+                                            ),
+                                            Icon(
+                                              Icons.arrow_forward_ios_rounded,
+                                              color: primaryColor,
+                                              size: 18,
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              "Order ID",
+                                              style: historyTextstyle,
+                                            ),
+                                            Text(
+                                              '${item.id!}',
+                                              style: historyTextstyle,
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              "Status",
+                                              style: historyTextstyle,
+                                            ),
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                              children: [
+                                                Icon(
+                                                  Icons.cancel,
+                                                  color: Colors.red,
+                                                  size: 15,
                                                 ),
-                                              ),
-                                            ],
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            "Total",
-                                            style: historyTextstyle,
-                                          ),
-                                          Text(
-                                            "\$ ${item.totalPrice!}",
-                                            style: historyTextstyle,
-                                          ),
-                                        ],
-                                      ),
-                                    ]))),
-                      );
+                                                Padding(
+                                                  padding: EdgeInsets.only(left: 10.0),
+                                                  child: Text(
+                                                    item.status!,
+                                                    style: historyTextstyle,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              "Total",
+                                              style: historyTextstyle,
+                                            ),
+                                            Text(
+                                              "\$ ${item.totalPrice!}",
+                                              style: historyTextstyle,
+                                            ),
+                                          ],
+                                        ),
+                                      ]))),
+                        );
                     }),
                     pagingController: _pagingController,
-                  ))
+
+                  ),
+                      ))
                 ],
               ),
             ),
--- lib/Screens/Widgets/loading_widget.dart
+++ lib/Screens/Widgets/loading_widget.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingWidget extends StatelessWidget {
   final Widget child;
   LoadingWidget({required this.child});
+  final spinKit = SpinKitRotatingCircle(
+    color: Colors.white,
+    size: 50.0,
+  );
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -10,7 +15,7 @@ class LoadingWidget extends StatelessWidget {
         Positioned.fill(child: child),
         Align(
           alignment: Alignment.center,
-          child: CircularProgressIndicator(),
+          child: spinKit,
         ),
       ],
     );
